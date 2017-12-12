e7697203130f7fc8095daae27109734b83de2a72
Zhentian Wan <answer881215@gmail.com>
2017-12-12 21:27:20 +0200

    Render prop

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Toggle from './Toggle';
+import {Switch} from './Switch';
 
 const App = () =>
-    <Toggle onToggle={(on) => console.log("Toggle", on)}>
-        <Toggle.On>
-            Switch is On!
-        </Toggle.On>
-        <Toggle.Button />
-        <Toggle.Off>
-            Switch is Off!
-        </Toggle.Off>
-    </Toggle>;
+    <Toggle
+        onToggle={(on) => console.log("Toggle", on)}
+        render={({on, toggle}) => (
+            <div>
+                {on
+                    ? 'The button is on'
+                    : 'The button is off'
+                }
+                <Switch on={on} onClick={toggle} />
+            </div>
+        )}
+    />;
 export default App;
