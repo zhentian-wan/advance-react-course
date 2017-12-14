a10c3788a469f2144ff419875d4eb8d6985a4242
Zhentian Wan <answer881215@gmail.com>
2017-12-14 15:54:32 +0200

    controlled props

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -2,31 +2,44 @@ import React from 'react';
 
 class Toggle extends React.Component {
 
-  // An empty function
-  static defaultProps = {onToggle: () => {}};
+    // An empty function
+    static defaultProps = {
+        defaultOn: false,
+        onToggle: () => {
+        }
+    };
 
-  // default state value
-  state = {on: false};
+    // default state value
+    initialState = {on: this.props.defaultOn};
+    state = this.initialState;
 
-  // toggle method
-  toggle = () =>
-    this.setState(
-      ({on}) => ({on: !on}),
-      () => {
-        this.props.onToggle(this.state.on)
-      }
+    // toggle method
+    toggle = () =>
+        this.setState(
+            ({on}) => ({on: !on}),
+            () => {
+                this.props.onToggle(this.state.on)
+            }
+        );
+
+    reset = () => this.setState(
+        this.initialState
     );
 
-  render() {
-    return this.props.render({
-        on: this.state.on,
-        toggle: this.toggle,
-        toggleProps: {
-            'aria-expanded': this.state.on,
-            onClick: this.toggle,
-        }
-    });
-  }
+    render() {
+        return this.props.render({
+            on: this.state.on,
+            toggle: this.toggle,
+            reset: this.reset,
+            getProps: (props) => {
+                return {
+                    'aria-expanded': this.state.on,
+                    role: 'button',
+                    ...props
+                };
+            }
+        });
+    }
 }
 
 export default Toggle;
--- src/components/app.js
+++ src/components/app.js
@@ -2,21 +2,77 @@ import React from "react";
 import Toggle from './Toggle';
 import {Switch} from './Switch';
 
-const App = () =>
-    <Toggle
-        onToggle={(on) => console.log("Toggle", on)}
-        render={({on, toggle, toggleProps}) => (
-            <div>
-                {on
-                    ? 'The button is on'
-                    : 'The button is off'
-                }
-                <Switch on={on} {...toggleProps} />
-                <hr />
-                <button {...toggleProps}>
-                    {on ? 'on': 'off'}
-                </button>
-            </div>
-        )}
-    />;
+class App extends React.Component {
+    initialState = {
+        timesClicked: 0,
+        on: false
+    };
+
+    state = this.initialState;
+
+    handleToggle = () => {
+        const timesClicked = this.state.timesClicked + 1;
+        this.setState({
+            timesClicked
+        });
+    };
+
+    cleanTimes = () => {
+        this.setState(
+            this.initialState
+        )
+    };
+
+    render() {
+        const {on, timesClicked} = this.state;
+        return (
+            <Toggle
+                on={on}
+                defaultOn={true}
+                onToggle={(on) => {
+                    console.log("Toggle", on);
+                    this.handleToggle();
+                }}
+                render={({on, toggle, getProps, reset}) => (
+                    <div>
+                        {on
+                            ? 'The button is on'
+                            : 'The button is off'
+                        }
+                        <Switch on={on} onClick={() => toggle()} {...getProps()} />
+                        <hr/>
+                        {
+                            timesClicked >= 4 ?
+                                (
+                                    <div>
+                                        <p>
+                                            <b>'You have click too many times</b>
+                                        </p>
+                                        <button
+                                            {...getProps({
+                                                'aria-label': 'Reset'
+                                            })}
+                                            onClick={
+                                                () => {
+                                                    reset();
+                                                    this.cleanTimes();
+                                                }
+                                            }>
+                                            Reset
+                                        </button>
+                                    </div>
+                                ) :
+                                (
+                                    <div>
+                                        You have clicked {timesClicked} times
+                                    </div>
+                                )
+                        }
+                    </div>
+                )}
+            />
+        );
+    }
+}
+
 export default App;
